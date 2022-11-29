adfdac61e29db62233265b3596596bbb7ae183a6
jycouet <jycouet@gmail.com>
2022-11-29 12:46:19 +0100

    :construction: NEW: isuue minimal repro

---
+++ src/lib/Collapsible.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let isActive = true;
+
+  const toggleIsActive = () => {
+    isActive = !isActive;
+  };
+</script>
+
+<div class="px-4 flex space-x-4">
+  {#if isActive}
+    <button on:click={toggleIsActive}> REMOVE </button>
+  {:else}
+    <button on:click={toggleIsActive}> ADD </button>
+  {/if}
+</div>
+
+{#if isActive}
+  <slot />
+{/if}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Collapsible from "$lib/Collapsible.svelte";
+
+  let isActive = false;
+</script>
+
+<Collapsible bind:isActive>HELLO</Collapsible>
